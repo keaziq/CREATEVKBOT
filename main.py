b4fc32fe2428dae6cdf722936a431924206c206c
Roman <40473552+keaziq@users.noreply.github.com>
2024-05-05 20:41:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ class Database(BaseStateGroup):
     ABOUT = 2
 
 @bot.on.message(text=hello)
-@bot.on.message(payload={"cmd": "hello"})
+@bot.on.message(payload={"brand": "hello"})
 async def message_welcome(message: Message):
     keyboard = Keyboard(inline=True)
-    keyboard.add(Text(f"Меню", {"cmd": "menu"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.add(Text(f"Меню", {"brand": "menu"}), color=KeyboardButtonColor.PRIMARY)
     # keyboard.add(Text(f"Оформить заказ", {"cmd": "zakaz"}), color=KeyboardButtonColor.PRIMARY)
     # keyboard.row()
     # keyboard.row()
@@ -31,13 +31,13 @@ async def message_welcome(message: Message):
                          f" Что вы хотите сделать сегодня?", keyboard=keyboard)
 
 @bot.on.message(text="меню")
-@bot.on.message(payload={"cmd": "menu"})
+@bot.on.message(payload={"brand": "menu"})
 async def menu(message: Message):
     keyboard = Keyboard(one_time=True)
 
-    keyboard.add(Text(f"Посмотреть ассортимент товаров", {"cmd": "assortment"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.add(Text(f"Посмотреть ассортимент товаров", {"brand": "assortment"}), color=KeyboardButtonColor.PRIMARY)
     keyboard.row()
-    keyboard.add(Text("Развлечение", {"cmd": "Развлечение"}), color=KeyboardButtonColor.SECONDARY)
+    keyboard.add(Text("Развлечение", {"brand": "Развлечение"}), color=KeyboardButtonColor.SECONDARY)
     keyboard.row()
     keyboard.add(Location())
     keyboard.row()
@@ -74,25 +74,25 @@ async def menu(message: Message):
 
 
 
-@bot.on.message(payload={"cmd": "assortment"})
+@bot.on.message(payload={"brand": "assortment"})
 async def carousel_handler(message: Message):
 
-    keyboard = Keyboard().add(Text("Подробнее", {"cmd": "shoes"}), color=KeyboardButtonColor.SECONDARY)
-    keyboard2 = Keyboard().add(Text("Подробнее", {"cmd": "shoes"}), color=KeyboardButtonColor.SECONDARY)
-    keyboard3 = Keyboard().add(Text("Подробнее", {"cmd": "shoes"}), color=KeyboardButtonColor.NEGATIVE)
+    keyboard_shoes = Keyboard().add(Text("Подробнее", {"brand": "shoes"}), color=KeyboardButtonColor.SECONDARY)
+    keyboard_clothes = Keyboard().add(Text("Подробнее", {"brand": "clothes"}), color=KeyboardButtonColor.SECONDARY)
+    keyboard3 = Keyboard().add(Text("Подробнее", {"brand": "shoes"}), color=KeyboardButtonColor.NEGATIVE)
 
     carousel = template_gen(
         TemplateElement(
             "Наш ассортимент обуви",
-            "Просмотрите бренды обуви",
+            "Чтобы продолжить нажми 'Подробнее' ",
             "-224358783_456239163",
-            keyboard.get_json()
+            keyboard_shoes.get_json()
         ),
         TemplateElement(
-            "Товар-2",
-            "1250 Р",
+            "Наш ассортимент одежды",
+            "Чтобы продолжить нажми 'Подробнее'",
             "-224358783_456239163",
-            keyboard2.get_json()
+            keyboard_clothes.get_json()
         ),
         TemplateElement(
             "Товар-3",
@@ -106,10 +106,10 @@ async def carousel_handler(message: Message):
 
 async def carouselback_handler(message: Message):
     keyboard = Keyboard(one_time=True)
-    keyboard.add(Text("Назад", {"cmd": "menu"}), color=KeyboardButtonColor.NEGATIVE)
+    keyboard.add(Text("Назад", {"brand": "menu"}), color=KeyboardButtonColor.NEGATIVE)
     await message.answer(keyboard=keyboard)
 
-@bot.on.message(payload={"cmd": "shoes"})
+@bot.on.message(payload={"brand": "shoes"})
 async def shoes_handler(message: Message):
     keyboard = Keyboard(one_time=True)
 
@@ -126,32 +126,51 @@ async def shoes_handler(message: Message):
 
     await message.answer("Выберите бренд:", keyboard=keyboard)
 
+@bot.on.message(payload={"brand": "clothes"})
+async def shoes_handler(message: Message):
+    keyboard = Keyboard(one_time=True)
+
+    keyboard.add(Text("Худи", {"brand": "NewBalance"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("Футболки", {"brand": "Nike"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("Свитеры", {"brand": "Adidas"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("Кофты", {"brand": "Puma"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("Куртки", {"brand": "Reebok"}), color=KeyboardButtonColor.PRIMARY)
+
+
+    await message.answer("Выберите бренд:", keyboard=keyboard)
+
 @bot.on.message(payload={"brand": "NewBalance"})
 async def newbalance_handler(message: Message):
 
-    keyboard = Keyboard().add(Text("Оформить заказ", {"cmd": "zakaz"}), color=KeyboardButtonColor.NEGATIVE)
-
+    keyboard574 = Keyboard().add(Text("Выбрать расцветку пары", {"brand": "colornb574"}), color=KeyboardButtonColor.NEGATIVE)
+    keyboard327 = Keyboard().add(Text("Выбрать расцветку пары", {"brand": "сolornb327"}), color=KeyboardButtonColor.NEGATIVE)
     carousel = template_gen(
-        TemplateElement(
-            "New Balance 327",
-            "Стильная и удобная обувь для ежедневной носки",
-            "-224358783_456239163",
-            keyboard.get_json(),
-
-        ),
         TemplateElement(
             "New Balance 574",
             "Классическая обувь для спорта и активного отдыха",
-            "-224358783_456239163",
-            keyboard.get_json()
+            "-224358783_456239167",
+            keyboard574.get_json()
+        ),
+        TemplateElement(
+            "New Balance 327",
+            "Для повседневного использования, не предназначены для спортивных целей.",
+            "-224358783_456239169",
+            keyboard327.get_json()
         )
+
     )
     await message.answer("Вы можете ознакомиться с брендом обуви NEW BALANCE. ", template=carousel)
 
-@bot.on.message(payload={"brand": "Nike"})
-async def newbalance_handler(message: Message):
 
-    keyboard = Keyboard().add(Text("Оформить заказ", {"cmd": "zakaz"}), color=KeyboardButtonColor.NEGATIVE)
+
+@bot.on.message(payload={"brand": "Nike"})
+async def nike_handler(message: Message):
+
+    keyboard = Keyboard().add(Text("Оформить заказ", {"brand": "zakaz"}), color=KeyboardButtonColor.NEGATIVE)
 
     carousel = template_gen(
         TemplateElement(
@@ -169,4 +188,94 @@ async def newbalance_handler(message: Message):
         )
     )
     await message.answer("Вы можете ознакомиться с брендом обуви NIKE. ", template=carousel)
+
+# NEWBALANCE #
+
+@bot.on.message(payload={"brand": "colornb574"})
+async def newbalance_color_handler_574(message: Message):
+    keyboard = Keyboard().add(Text("Перейти к выбору размера", {"brand": "size"}), color=KeyboardButtonColor.NEGATIVE)
+
+    carousel = template_gen(
+        TemplateElement(
+            "New Balance 574",
+            "Brown with royal blue",
+            "-224358783_456239167",
+            keyboard.get_json()
+        ),
+        TemplateElement(
+            "New Balance 574",
+            "Navy with green",
+            "-224358783_456239168",
+            keyboard.get_json()
+        ),
+        TemplateElement(
+            "New Balance 574",
+            "Black with grey",
+            "-224358783_456239166",
+            keyboard.get_json()
+        )
+    )
+
+    await message.answer("В наличии находятся ниже представленные пары NEW BALANCE. ", template=carousel)
+
+@bot.on.message(payload={"brand": "сolornb327"})
+async def newbalance_color_handler_327(message: Message):
+    keyboard = Keyboard().add(Text("Перейти к выбору размера", {"brand": "size"}), color=KeyboardButtonColor.NEGATIVE)
+
+
+
+    carousel = template_gen(
+        TemplateElement(
+            "New Balance 327",
+            "Brighton grey with slate grey",
+            "-224358783_456239169",
+            keyboard.get_json()
+        ),
+        TemplateElement(
+            "New Balance 327",
+            "Sea salt with moonrock",
+            "-224358783_456239170",
+            keyboard.get_json()
+        ),
+        TemplateElement(
+            "New Balance 327",
+            "Chrome blue with light chrome blue",
+            "-224358783_456239171",
+            keyboard.get_json()
+        )
+    )
+
+    await message.answer("В наличии находятся ниже представленные пары NEW BALANCE 327. ", template=carousel)
+
+@bot.on.message(payload={"brand": "size"})
+async def size_handler(message: Message):
+    keyboard = Keyboard(one_time=True)
+
+    keyboard.add(Text("36-36.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("37-37.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("38-38.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("39-39.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("40-40.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("41-41.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("42-42.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+    keyboard.row()
+    keyboard.add(Text("44-44.5", {"brand": "basket"}), color=KeyboardButtonColor.PRIMARY)
+
+    await message.answer("Выберите размер:", keyboard=keyboard)
+
+@bot.on.message(payload={"brand": "basket"})
+async def basket(message: Message):
+    keyboard = Keyboard(one_time=True)
+
+    await message.answer("Удачно добавленно в корзину", keyboard=keyboard)
+
+
+
 bot.run_forever()
+
