4c8038d5904c2f08cb0d29a8b057a95f0496b497
Roman <40473552+keaziq@users.noreply.github.com>
2024-05-10 16:56:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from vkbottle.bot import Bot, Message
 
 
 
-bot = Bot(token=token)
-token = "vk1.a.hHMpk86LD-qDpLrscN9vz-NV5gYnf5QIU3Cz5-tJAdZdqKP92B2c_XfAtRF5M3rcyUVtr3dl2OG0MPLEwKD0tbj2fcVYF56HCcQx7ZbMBXc8yIWhJSPb_BrJutS6wk0iWRr1_PJalVlp2QveywAdugnYmc6cVZrcfyaPtCWecyoGLGEToTioN-J8Nxc8q4Duy8mlUK--NtZxH24R7v3xxA"
+bot = Bot(token = "vk1.a.hHMpk86LD-qDpLrscN9vz-NV5gYnf5QIU3Cz5-tJAdZdqKP92B2c_XfAtRF5M3rcyUVtr3dl2OG0MPLEwKD0tbj2fcVYF56HCcQx7ZbMBXc8yIWhJSPb_BrJutS6wk0iWRr1_PJalVlp2QveywAdugnYmc6cVZrcfyaPtCWecyoGLGEToTioN-J8Nxc8q4Duy8mlUK--NtZxH24R7v3xxA")
+
 ctx = CtxStorage()
 
 hello = ["Привет","привет", 'start', 'Хай','хай', 'Ку-ку', 'здарова','Здарова',
